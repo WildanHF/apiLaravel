712edbb21363f22d4ad8a9016c433039a33cc697
WildanHF <whadi611@gmail.com>
2024-12-26 15:37:34 +0700

    api registrasi login terbaru

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::post('/registrasi', [UserController::class,'register']);
+Route::post('/login', [UserController::class,'login']);
